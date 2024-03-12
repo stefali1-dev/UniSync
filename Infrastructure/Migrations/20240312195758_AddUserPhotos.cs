1340ba9f3744ab5d13f56178dad6b7181647c905
stefali1-dev <stefanleustean56@gmail.com>
2024-03-12 22:17:22 +0200

    update databases

---
+++ Infrastructure/Migrations/20240312195758_AddUserPhotos.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniSync.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPhotos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Courses_Professor_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Professor_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Professor_CourseId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateTable(
+                name: "CourseStudent",
+                columns: table => new
+                {
+                    CoursesCourseId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentsUserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseStudent", x => new { x.CoursesCourseId, x.StudentsUserId });
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Courses_CoursesCourseId",
+                        column: x => x.CoursesCourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Users_StudentsUserId",
+                        column: x => x.StudentsUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PasswordResetCodes",
+                columns: table => new
+                {
+                    PasswordResetCodeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    ExpirationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResetCodes", x => x.PasswordResetCodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPhotos",
+                columns: table => new
+                {
+                    UserPhotoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PhotoUrl = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPhotos", x => x.UserPhotoId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseStudent_StudentsUserId",
+                table: "CourseStudent",
+                column: "StudentsUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseStudent");
+
+            migrationBuilder.DropTable(
+                name: "PasswordResetCodes");
+
+            migrationBuilder.DropTable(
+                name: "UserPhotos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Professor_CourseId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Professor_CourseId",
+                table: "Users",
+                column: "Professor_CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Courses_Professor_CourseId",
+                table: "Users",
+                column: "Professor_CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId");
+        }
+    }
+}
---
+++ UniSync.Identity/Migrations/20240312201522_AddUserProperties.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniSync.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Facebook",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GitHub",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedIn",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Facebook",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GitHub",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedIn",
+                table: "AspNetUsers");
+        }
+    }
+}
